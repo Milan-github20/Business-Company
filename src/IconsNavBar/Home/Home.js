b33741cb85c8213c1d170a8658d36507849a3aaf
Milan-github20 <80204547+Milan-github20@users.noreply.github.com>
2022-11-25 15:36:12 +0100

    Add files via upload

--- src/IconsNavBar/Home/Home.js
+++ src/IconsNavBar/Home/Home.js
@@ -6,23 +6,25 @@ const Home = (props) => {
     <div className={styles.div}>
       <div className={styles.div_div}>
         <div className={styles.div_div_div}>
-          <h1>
-            Welcome to <span className={styles.span}>BUSINESS</span> COMPANY
-          </h1>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus
-            tempore accusamus quis magnam doloremque itaque ad modi, pariatur
-            iste labore similique officiis impedit aspernatur aperiam facere
-            architecto. Eligendi, repellendus inventore!
-          </p>
-          <div className={styles.workersAndBoss}>
-            <div className={styles.workers}>
-              <img src="./images/people.png" alt="" />
-              <p>More than 50 Workers</p>
-            </div>
-            <div className={styles.bosses}>
-              <img src="./images/diplomaHome.png" alt="" />
-              <p>More than 20 Bosses</p>
+          <div className={styles.page}>
+            <h1>
+              Welcome to <span className={styles.span}>BUSINESS</span> COMPANY
+            </h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus
+              tempore accusamus quis magnam doloremque itaque ad modi, pariatur
+              iste labore similique officiis impedit aspernatur aperiam facere
+              architecto. Eligendi, repellendus inventore!
+            </p>
+            <div className={styles.workersAndBoss}>
+              <div className={styles.workers}>
+                <img src="./images/people.png" alt="" />
+                <p>More than 50 Workers</p>
+              </div>
+              <div className={styles.bosses}>
+                <img src="./images/diplomaHome.png" alt="" />
+                <p>More than 20 Bosses</p>
+              </div>
             </div>
             <a>
               <button className={styles.btn}>About us</button>
